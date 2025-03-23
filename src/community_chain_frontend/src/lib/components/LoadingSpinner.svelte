ef2b149f07b798cec3c2b861243e2cea235318af
WHXisWH <whx12110405@gmail.com>
2025-03-23 15:41:36 +0900

    Create LoadingSpinner.svelte

---
+++ src/community_chain_frontend/src/lib/components/LoadingSpinner.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let size = '2rem';
+</script>
+
+<div class="spinner" style="width: {size}; height: {size};">
+  <div class="bounce1"></div>
+  <div class="bounce2"></div>
+  <div class="bounce3"></div>
+</div>
+
+<style>
+  /* スタイル省略 - 実際の実装ではここに適切なCSSを記述 */
+</style>
